a814e0fd65e809ef130bb36474c16d8e996d86ab
Ahmed Khusaam <hello@khusaam.com>
2017-03-03 03:54:19 +0500

    reorganize code

---
+++ src/Pay.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace aharen;
+
+class Pay
+{
+    public function __construct()
+    {
+        dd('asd sss');
+    }
+    // protected $provider;
+
+    // public function __construct()
+    // {
+    // }
+
+    // protected function construct(string $provider)
+    // {
+    //     $this->provider = $this->makeProvider($provider);
+    // }
+
+    // protected function makeProvider(string $provider)
+    // {
+    //     $providerClass = "\aharen\Pay\Providers\$provider";
+    //     return get_class(new $providerClass());
+    // }
+
+    // public function make($config)
+    // {
+    //     $this->construct();
+    // }
+}
