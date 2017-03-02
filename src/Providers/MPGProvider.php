8f1b868d009e0ad012a4965370a4d6ba249aa5e1
Ahmed Khusaam <hello@khusaam.com>
2017-03-03 03:30:04 +0500

    provider created and gateway created

--- src/Providers/MPGProvider.php
+++ src/Providers/MPGProvider.php
@@ -4,5 +4,18 @@ namespace aharen\Pay\Providers;
 
 class MPGProvider extends AbstractProvider
 {
-
+    protected function configRules()
+    {
+        return [
+            'Host'                     => true,
+            'PurchaseCurrency'         => false,
+            'PurchaseCurrencyExponent' => false,
+            'Version'                  => false,
+            'SignatureMethod'          => false,
+            'AcqID'                    => true,
+            'MerID'                    => true,
+            'PurchaseAmt'              => true,
+            'OrderID'                  => true,
+        ];
+    }
 }
--- tests/PayTest.php
+++ tests/PayTest.php
@@ -4,5 +4,6 @@ class PayTest extends \PHPUnit_Framework_TestCase
 {
     public function testMPGSignature()
     {
+
     }
 }
